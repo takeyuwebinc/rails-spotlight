de010e772ad0b7e43e3cbcd628517213bd076d8a
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2025-05-25 09:02:43 +0000

    fix: 記事の少ないタグは表示しない

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     @featured_tags = Tag.joins(:articles)
                         .where("articles.published_at <= ?", Time.current)
                         .group("tags.id")
+                        .having("COUNT(articles.id) >= ?", 3)
                         .order("COUNT(articles.id) DESC")
                         .limit(5)
   end
